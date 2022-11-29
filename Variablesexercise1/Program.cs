5872b642eea6cb3aa0fa4a692258d9eba012c592
Taurus Anderson <imtaurus97@gmail.com>
2022-11-29 10:25:49 -0600

    Second Commit

---
+++ Variablesexercise1/Program.cs
@@ -0,0 +1,13 @@
+﻿string name = "Alice";
+int age = 19;
+char middleInitial = 'L';
+bool isOver18 = true;
+double wonderLandTemp = 75.8;
+decimal wonderLandMoney = 8.35m;
+
+Console.WriteLine($"Name {name}");
+Console.WriteLine($"Age {age}");
+Console.WriteLine($"Middle Initial {middleInitial}");
+Console.WriteLine($"Is this person over 18 {isOver18}");
+Console.WriteLine($"The tempature in wonderland is about {wonderLandTemp}");
+Console.WriteLine($"She has about {wonderLandMoney} money saved up");
